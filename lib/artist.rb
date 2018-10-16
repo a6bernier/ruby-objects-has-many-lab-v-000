89c3c45e8d691b6685bb26ca77ed471aa19cc10d
Andrew Bernier <a6bernier@gmail.com>
2018-10-16 19:11:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,30 @@
+
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@song_count = 0
 
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
+    @songs = []
   end
 
-   def songs
-    @songs
-  end
-
-   def add_song(song)
+  def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
 
-   def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
 
-   def self.song_count
+  def self.song_count
     @@song_count
   end
 
- end
+end
