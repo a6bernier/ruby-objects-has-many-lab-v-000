4de751f30c948d35220ab5bf05124516e8214ee4
Andrew Bernier <a6bernier@gmail.com>
2018-10-16 19:11:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,26 @@
+
 class Song
+
+  attr_accessor :name, :artist
+
   @@all = []
-  
-  attr_accessor :name, :artist, :genre
-   def initialize(name:, artist: = nil, genre: = nil)
+
+  def initialize(name, artist=nil)
     @name = name
-    self.artist=(artist) if artist
-    @genre = genre
-  end
-  
-  def artist=(artist_name)
-    if Artist.find_by_name(artist_name)
-      Artist.find_by_name(artist_name).songs << self
-      @artist = Artist.find_by_name(artist_name)
-    else
-      new_artist = Artist.create(artist_name)
-      new_artist.songs << self
-      @artist = new_artist
-    end
-  end
-  
-  def create(name)
-    new_song = Song.new(name)
-    self.save
-  end
-  
-  def save
+    @artist = artist
     @@all << self
   end
-  
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
   def self.all
     @@all
   end
-  
-  def self.destroy_all
-    @@all.clear
-  end
- end
+
+end
